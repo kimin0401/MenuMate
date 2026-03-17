268379f711af3edee8e63273a3c37c7f38b5c2d6
KimIn <inptp1996@gmail.com>
2026-03-17 13:14:07 +0900

    Init: 검색창 파일 생성

---
+++ src/shared/ui/SearchBar.tsx
@@ -0,0 +1,12 @@
+export const SearchBar = () => {
+  return (
+    <form className="w-full">
+      <div className="flex items-center rounded-full border bg-white">
+        <input className="flex-1 bg-transparent" />
+        <button type="submit" className="shrink-0">
+          {/* <Search /> */}
+        </button>
+      </div>
+    </form>
+  );
+};
