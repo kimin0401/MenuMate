9b1abce27fd31d8813a6f405584b15cae9650fdc
KimIn <inptp1996@gmail.com>
2026-03-18 21:01:58 +0900

    feat:검색창 props 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { cn } from '@/shared/lib/cn';
 import { BrandLogo } from '@/shared/ui/brandLogo';
+import { SearchBar } from '@/shared/ui/SearchBar';
 
 export default function HomePage() {
   return (
     <main className="min-h-dvh px-4">
       <section className={cn('mx-auto grid min-h-dvh place-items-center py-16', 'max-w-screen-md')}>
         <BrandLogo />
+        <SearchBar />
       </section>
     </main>
   );
