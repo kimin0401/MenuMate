934d5920e4cbf7bad7cff6d7b9b1d834c745cc9b
KimIn <inptp1996@gmail.com>
2026-04-02 17:30:20 +0900

    fix: 이미지 처리 위해 next.config 확장 처리

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -9,6 +8,24 @@ const nextConfig: NextConfig = {
         hostname: 'www.foodsafetykorea.go.kr',
         port: '',
         pathname: '/uploadimg/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.foodsafetykorea.go.kr',
+        port: '',
+        pathname: '/uploadimg/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'www.foodsafetykorea.go.kr',
+        port: '',
+        pathname: '/common/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.foodsafetykorea.go.kr',
+        port: '',
+        pathname: '/common/**',
       },
     ],
   },
