fa00b96a65b3f3782b0485c0e07f12e6d2a14da3
KimIn <inptp1996@gmail.com>
2026-05-08 13:32:10 +0900

    refactor: 브랜드 로고 props 추가 버전으로 리팩토링

--- src/shared/ui/BrandLogo.tsx
+++ src/shared/ui/BrandLogo.tsx
@@ -1,9 +1,15 @@
-export const BrandLogo = () => {
+import { cn } from '@/shared/lib/cn';
+
+type BrandLogoProps = {
+  className?: string;
+};
+
+export const BrandLogo = ({ className }: BrandLogoProps) => {
   return (
     <img
       src="/logo/MenuMateLogo.svg"
       alt="MenuMate logo"
-      className="h-12 w-auto select-none md:h-16 lg:h-20"
+      className={cn('h-10 w-auto select-none', className)}
     />
   );
 };
